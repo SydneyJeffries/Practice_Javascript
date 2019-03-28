c14dcb37450d87f9ff23f0f5b40b73f1f01a4c53
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-03-28 14:11:55 -0500

    Create Index2.js

---
+++ Index2.js
@@ -0,0 +1,16 @@
+/////This is a doc from a class I took on Udmey with instructor Mosh Hamedani on Object orriented programing in Javascript. 
+
+
+/////////section 5 es6 classes //////
+
+///es6 classes create a new way to create objects in JS. they are not classes like c# and java, they are just snytactic sugar. 
+
+function Circle (radius){
+    this.radius
+    
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+
