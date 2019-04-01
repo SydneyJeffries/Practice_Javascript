d973a7253dfa44d35c9e19808c380e7a41a7bed0
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-04-01 18:17:34 -0500

    Update test.js

---
+++ test.js
@@ -0,0 +1,48 @@
+/////////////////////////////Exercise //////////////
+// a stack uses push to add and pop to remove.
+// pop from the top
+// the instance of the stack class  we create will have a count property. the count property will tell me how many objects are in the stack.
+// it's imediate prototype will be an object class
+// the prototype will have  3 methods: peek, pop and push
+// peek will return the object to the top of the stack but it does not remove it from the stack. it allows us to see what's on top of the stack.
+// we will get errors if we call pop and peek on empty stacks. 
+
+
+const _items = new WeakMap();
+
+class Stack {
+
+constructor() {
+
+    _items.set(this, []);
+
+    get count() {
+       return _items.get(this).length;
+    }
+
+    }
+    
+    push(obj) {
+    
+        _items.get(this).push(obj);
+
+    }
+
+    peek() { 
+        const items = _ items.get(this);
+
+        if (_items.length === 0 )
+         throw new Error('this stack is empty');
+
+        return items.[items.length - 1 ];
+
+    }
+
+    pop(){
+        if (_items.get(this).length === 0 )
+            throw new Error ('stack is empty')
+        return _items.get(this).pop()
+
+    }
+
+}
