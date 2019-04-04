3e894451ad76c97110137c058ce0730fb17db49b
Sydney Jeffries <sydney.jeffriess@gmail.com>
2019-04-04 12:38:06 -0500

    arrays

---
+++ Object Orriented w js/FILTERING SERCHING SORTING/array.js
@@ -0,0 +1,24 @@
+// the items in an array are sometimes refered to as elements. 
+
+//adding items to an array: 
+ Push(); //pushes an element to the top of an array  
+ unshift(); // puts an element in the front of the line. it cuts. 
+
+ //removing elements from an array:
+ Pop(); // pop from the top
+ shift(); // removes the first element from the array. 
+
+ //iterating over the items in an array
+ // there are several ways to iterate over the items in an array such as using the Loops...
+ // or these build in array methods. 
+ // 
+ forEach(); // a function for each element in the array gets executed. 
+ map(); // map ccalls a function on each element in an array and creates a new array with results. 
+ 
+ some(); //checks that some elements in an array pass a test specfied by a function
+ every(); // checks if all elements in an array pass a test specified by a function
+
+ //combining
+
+ concat();  //creats a mew array containing this array and other arrays
+ 
